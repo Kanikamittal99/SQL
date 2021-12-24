9300dca743b007fc41685152ffacc0d9e1b24848
Kanika Mittal <kanikamittal99>
2021-12-24 23:02:51 +0530

    Initial commit

--- companyDB/union.sql
+++ companyDB/union.sql
@@ -1,11 +1,11 @@
 -- UNION: should have same number of columns and similar datatype
 
 -- Find a list of employee and branch names
-select first_name as company_names
-from employee
-union
-select branch_name
-from branch;
+SELECT first_name AS company_names
+FROM employee
+UNION
+SELECT branch_name
+FROM branch;
 
 -- Find a list of all clients & branch suppliers' names
 SELECT client_name , client.branch_id 
@@ -15,8 +15,8 @@ SELECT supplier_name, branch_supplier.branch_id
 FROM branch_supplier;
 
 -- Find a list of all money spent or earned by the company
-select salary 
-from employee
-union
-select total_sales 
-from works_with
+SELECT salary 
+FROM employee
+UNION
+SELECT total_sales 
+FROM works_with
