950cc64fc5ba7e1c662fb470e97f0a787da73e0f
Kanika Mittal <kanikamittal99>
2021-12-24 23:02:19 +0530

    Initial commit

--- companyDB/onDelete.sql
+++ companyDB/onDelete.sql
@@ -2,23 +2,23 @@
 -- Since, Foreign key(mgr_id) in branch table was set to ON DELETE SET NULL
 -- Only the mgr_id changes to NULL when emp_id is deleted from parent table(employee)
 
-delete from employee 
-where emp_id = 102;
+DELETE FROM employee 
+WHERE emp_id = 102;
 
-select *
-from branch;
+SELECT *
+FROM branch;
 
 -- Foreign key(super_id) changes to null for employee with emp_id = 2 too. 
-select * from employee;
+SELECT * FROM employee;
 
 
 -- ON DELETE CASCADE
 -- deleting primary key(branch_id) of branch table
-delete from branch 
-where branch_id = 2;
+DELETE FROM branch 
+WHERE branch_id = 2;
 
 --  whole rows with foreign key(branch_id) = 2 gets removed.
-select * 
+SELECT * 
 FROM branch_supplier;
 
 
